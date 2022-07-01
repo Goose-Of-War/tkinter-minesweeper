8628d4efa796c50717e5e08547a446afaedc2a98
Vidunram A R <64352864+FriedPotato03@users.noreply.github.com>
2022-07-01 16:55:32 +0530

    Commit 2
    Added window + click button work (half-way)
    
    Updated README

--- README.md
+++ README.md
@@ -1,27 +1,34 @@
 # tkinter-minesweeper
  Making a Minesweeper in Python using tkinter
 
-##
+## 
 Hello once again, from your dear ol' Goos. This one is something which I did in the beginning of the lockdown. I was interested in Minesweeper around the beginning of 2020 and seeing how simple it was, I wanted to make a copy of the same. 
 
 In the lockdown, I made 2 copies of the game - one being a shabby mess of a code, while the other being a wee bit better than the first one. In case you want to see the 2nd edition, [click here](https://pastebin.com/mmze0P8E).
 
-##
+#
 ### Will it be the same thing just copied again?
 I hope not. Otherwise, why bother, am I right? :sweat_smile: I'll most probably do some tweaking and make it a bit more definite rather than my older code which... I _do_ understand, just, after reading it 7 times. 
 
 This time again, I will be using `tkinter`, with some other libraries and packages like `random` or any other thing I think might be needed.
 
-##
+#
 ### What's done till now, Goose?
-Well, as of now, nothing significant is done yet. All I have on me is the grid designed and an empty click function. 
+Well, as of now, I have the grid ready, and a window launched when I run the program, with all the buttons. If you click the buttons,  the number of mines near said tile will be displayed (i.e. of course, *if* the location is not a mine. If it is, then you get a **F**)
 
 ### What's coming soon?
-Hopefully, the window with all the buttons.
+Now that we have a definite button system, the upcoming tasks are somewhat clearer now. Here's what I have to do next.
+- A flag system, to mark the buttons with mines (involving a button, a flag counter, and the code for the flag.)
+- A second-click system which makes clearing multiple buttons at one go convenient. 
+- A reset button, to restart the game if you want to.
+- A victory/defeat alert system, to announce if you won or not (even tho it becomes evident by the gameplay.)
 
-##
-This project is currently being updated. Until next time.
+#
+Well, until next time, I guess.
 
 Honk
 
 :swan:
+
+*As this project is being updated in a somewhat regular basis, there may be discrepancies in the README file and the actual project work.*
+
--- main.py
+++ main.py
@@ -2,28 +2,60 @@ import tkinter as tk
 from random import randint as rand
 from tkinter import messagebox
 
-def generate_grid(grid_object: dict, coord: tuple):
+def start_game():
+	'''Generates grid for buttons.'''
+	#Need to add tkinter command too
+	global ROWS, COLS, GRID, GAME_STATE, WINDOW
+	GRID.clear()
+	for object in WINDOW.grid_slaves():
+		del object
+	tk.Label(WINDOW, text='Minesweeper').grid(row=0, column=0, columnspan=COLS)
+	for i in range(ROWS):
+		for j in range(COLS):
+			GRID[(i,j)] = {'opened': False, 'mine': False, 'neighbour': 0, 'button': eval('tk.Button(WINDOW, width=2, height=1, command=lambda: click(({},{})), text="")'.format(i,j))} #Replacing this with an exec command, and then adding the button to this dict.
+			GRID[(i,j)]['button'].grid(row=i+1, column=j)
+	GAME_STATE = 'FIRST-CLICK'
+
+def generate_mines(coord: tuple):
 	'''Generates a grid object based on the first clicked co-ordinate.\n The coordinates in a (1,1) distance from them will not be given any mines.'''
-	global rows, columns, number_of_mines
+	global ROWS, COLS, NUM_MINES, GRID, GAME_STATE
 	s = set()
-	while len(s)!=number_of_mines:
-		mine = (rand(0, rows - 1), rand(0, columns-1))
-		if abs(mine[0]-coord[0]) and abs(mine[1]-coord[1]):
+	while len(s)!=NUM_MINES:
+		mine = (rand(0, ROWS - 1), rand(0, COLS-1))
+		if abs(mine[0]-coord[0])>1 and abs(mine[1]-coord[1])>1:
 			s.add(mine)
-	for i in range(rows):
-		for j in range(columns):
-			grid_object[(i,j)]['mine'] = (i,j) in s
-			grid_object[(i,j)]['opened'] = False
-	click(coord)
-	
+	print(s)
+	for i in range(ROWS):
+		for j in range(COLS):
+			GRID[(i,j)]['mine'] = (i,j) in s
+			GRID[(i,j)]['opened'] = False
+			GRID[(i,j)]['neighbour'] = [(I,J) for I in range(max(0,i-1), min(i+2,ROWS)) for J in range(max(0,j-1),min(j+2,COLS)) if (I,J) in s and (I!=i or J!=j)]
+			print(len( GRID[(i,j)]['neighbour']) if not GRID[(i,j)]['mine'] else 'F', end=" ")
+		print()
+	GAME_STATE = 'STARTED'
 
 def click(coord: tuple): 
 	'''Called on clicking a button. Depending on the button's attributes, different actions will happen.'''
-	pass
+	global GAME_STATE, click_set
+	if GAME_STATE == 'FIRST-CLICK':
+		generate_mines(coord)
+	
+	GRID[coord]['opened'] = True
+	if GRID[coord]['mine']: 
+		GRID[coord]['button']['text'] = "F"
+	else:
+		GRID[coord]['button']['text'] = len(GRID[coord]['neighbour'])
+		print(coord, GRID[coord]['neighbour'], sep=':')
+	#Nothing concrete has been filled yet. Will do that part soon.
 
 GAME_STATE = 'FIRST-CLICK'
 FLAG_STATE = False
-rows = 15
-columns = 8
-number_of_mines = 20
+ROWS = 15
+COLS = 12
+NUM_MINES = 24
 GRID = {}
+WINDOW = tk.Tk()
+WINDOW.title("Minesweeper")
+
+start_game()
+WINDOW.mainloop()
